e8d1d429c09619c5850bbf6ed1c0080c87465feb
Rahul Patwal <patwalrahul3@gmail.com>
2023-12-10 12:31:40 +0530

    added envri variable

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,5 @@
+import { Environment } from "./environment";
+
+export  const DevEnvironment: Environment = {
+    db_uri: 'mongodb+srv://lunatic07:rahul@cluster0.cfcgpvl.mongodb.net/swiggyClone'
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
+import { getEnvironmentVariables } from "./environments/environment";
 
 let app: express.Application = express();
 
 app.listen(3000, () =>{
     console.log(`listing to port 3000`);
 });
+mongoose.connect(getEnvironmentVariables().db_uri).then((res) =>{
+    console.log("connected Successfully !!!!!!!!")
+})
+// mongoose.connect('mongodb+srv://lunatic07:rahul@cluster0.cfcgpvl.mongodb.net/swiggyClone').then((res) =>{
+//     console.log("connected Successfully !!!!!!!!")
+// })
+ 
+
